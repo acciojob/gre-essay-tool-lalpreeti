bc7d7fd2ed3acfea549f12e83d6adb8436687e23
preeti <118329523+lalpreeti@users.noreply.github.com>
2023-02-25 16:39:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+let text=document.getElementById('evaluatedText');
+let header=document.getElementById('wordCount');
+text.addEventListener('keyup',function(){
+        let sentence=text.value;
+ 
+        let count=0, flag=false;
+        for(let i=0;i<sentence.length;i++){
+                if(sentence[i]==" "){
+                        count=count+1;
+                }
+                if(sentence[sentence.length-1]==" "){
+                        flag=true;
+                }
+        }
+        if(flag==false){
+                count=count+1;
+        }
+        header.innerHTML=count;
+        if(sentence.length==0){
+                header.innerHTML=0;
+        }
+});
+header.innerHTML=0;
+ 
